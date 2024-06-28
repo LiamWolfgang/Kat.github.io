2882f8a89b8ab50d080541eb97c4dd75f21a6137
LiamWolfgang <azore419@gmail.com>
2024-06-28 17:07:19 +0200

    sistema de carrito en proceso

---
+++ src/js/class/producto.js
@@ -0,0 +1,8 @@
+
+class producto{
+    constructor(nombre,descripcion,precio){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+}
---
+++ src/js/object.js
@@ -0,0 +1,19 @@
+import { producto } from './class/producto.js';
+
+
+
+
+
+
+// productos
+const platillo = new producto();
+const pinxo = new producto();
+const postre = new producto();
+const bebida = new producto();
+
+Object.assign(
+    platillo,{
+        nombre: 
+    }
+
+);
